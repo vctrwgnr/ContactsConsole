c8fef0d03f6318528bff7e5a059a862c6727262c
vctrwgnr <victoriaemich@gmail.com>
2025-03-14 08:47:24 +0100

    Initial commit

---
+++ ContactsConsole/Model/Contact.cs
@@ -0,0 +1,18 @@
+﻿namespace ContactsConsole.Model;
+
+public class Contact
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    
+    public Contact(string name, string surname, string email, string phone)
+    {
+        Name = name;
+        Surname = surname;
+        Email = email;
+        Phone = phone;
+        
+    }
+}
---
+++ ContactsConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using ContactsConsole.Model;
+
+namespace ContactsConsole;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Current Working Directory: " + Directory.GetCurrentDirectory());
+        string filePath = @"..\..\..\data\contacts.json";
+        try
+        {
+            string jsonResponse = await File.ReadAllTextAsync(filePath);
+            List<Contact> contacts = JsonSerializer.Deserialize<List<Contact>>(jsonResponse);
+            Console.WriteLine("name: |  surname: |  email: |  phone: |  address: |");
+            foreach (var contact in contacts)
+            {
+                
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine($"{contact.Name} | {contact.Surname} | {contact.Email} | {contact.Phone}");
+                
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+            throw;
+        }
+        
+
+        
+        
+    }
+}
